94ad0fb5293638f50f18006969585746a3d94708
Sam Poppe <poppe.samuel@gmail.com>
2022-04-26 17:12:43 -0700

    Added Styledstage.js
    created StyledStage component and fixed errrors in StyledCell component

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -4,7 +4,7 @@ import { TETROMINOS } from '../../tetrominos';
 
 const Cell = ({ type }) =>{
   return(
-  <StyledCell type={'L'} color={TETROMINOS['L'].color}>Cell</StyledCell>
+  <StyledCell type={'L'} color={TETROMINOS['L'].color}/>
   )
 }
 
--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Cell from '../Cell';
+import { StyledStage } from './styledStage';
 
 const Stage = ({ stage }) =>{
   return(
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
       {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
-    </div>
+    </StyledStage>
   )
 }
 
