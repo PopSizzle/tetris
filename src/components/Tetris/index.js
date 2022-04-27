8da9dd919bc0e183a4b96b908d6d923b8118e9e8
Sam Poppe <poppe.samuel@gmail.com>
2022-04-27 13:10:01 -0700

    Build out usePlayer and useStage hooks
    coded out the custom hooks of usePlayer and useStage

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { createStage } from '../../gameHelpers'; 
+
 // styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styledTetris';
 
@@ -18,13 +20,45 @@ const Tetris = () => {
   const[dropTime, setDropTime] = useState(null);
   const[gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log('re-render');
   
+  const movePlayer = (direction) =>{
+      updatePlayerPos({ x: direction, y: 0});
+  }
+
+  const startGame = () =>{
+    // reset to starting position
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () =>{
+    updatePlayerPos({ x: 0, y: 1, collided: false})
+  }
+
+  const dropPlayer = () =>{
+      drop();
+  }
+
+  const move = ({ keyCode }) =>{
+      if(!gameOver){
+        if(keyCode === 37) {
+          movePlayer(-1);
+        } else if(keyCode === 39){
+          movePlayer(1);
+        } else if(keyCode === 40){
+          dropPlayer();
+        }
+      }
+
+  }
+
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -37,7 +71,7 @@ const Tetris = () => {
             <Display text='Level' />
           </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame}/>
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
-export const useStage = () =>{
+export const useStage = (player, resetPlayer) => {
     const [stage,setStage] = useState(createStage());
+
+    useEffect(() => {
+
+      const updateStage = (prevStage) => {
+
+        // Clear from prev render
+        const newStage = prevStage.map(row =>
+          row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+          )
+
+          // Draw new stage with tetromino
+          player.tetromino.forEach((row, y) => {
+            row.forEach((value, x) => {
+              if(value !== 0) {
+                newStage[y + player.pos.y][x + player.pos.x] = [
+                  value,
+                  `${player.collided ? 'merged' : 'clear'}`
+                ];
+              }
+            });
+          });
+
+          return newStage;
+      };
+
+      setStage(prev => updateStage(prev));
+
+    }, [player])
 
     return[stage, setStage];
 }
