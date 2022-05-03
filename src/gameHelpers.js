e757af0ee7584100ab6dfde53dce9aef36f332b3
Sam Poppe <poppe.samuel@gmail.com>
2022-05-03 14:45:34 -0700

    fixed bug in sweepRows function
    fixed a bug in the sweepRows function due to not returning the correct variable

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -9,7 +9,7 @@ export const createStage = () => {
 }
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-  for(let y = 0; y < player.tetromino.length; y++) {
+  for(let y = 0; y < player.tetromino.length; y ++) {
     for(let x = 0; x < player.tetromino[y].length; x ++) {
 
       // check that we are on a shape
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -9,9 +9,9 @@ export const useStage = (player, resetPlayer) => {
 
       setRowsCleared(0);
 
-      const sweepRows = (newStage) => {
+      const sweepRows = newStage =>
         newStage.reduce((acc, row) =>{
-          if(row.findIndex(cell =>[0] === 0) === -1){
+          if(row.findIndex(cell => cell[0] === 0) === -1){
             setRowsCleared(prev => prev + 1);
             acc.unshift(new Array(newStage[0].length).fill([0,'clear']));
             return acc;
@@ -19,7 +19,6 @@ export const useStage = (player, resetPlayer) => {
           acc.push(row);
           return acc;
         }, [])
-      }
 
       const updateStage = (prevStage) => {
 
